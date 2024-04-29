025188c08590309dd5dc83f65adf28c292305463
Dr Peeper <pepperpr86@gmail.com>
2024-04-29 13:21:20 +0700

    added 3.14ey's and star's guns

---
+++ 314ey/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\314ey\sources"
+ren "H:\portalguns\314ey\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\314ey\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
---
+++ star/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\star\sources"
+ren "H:\portalguns\star\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\star\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
