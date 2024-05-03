2434b391cf944e225ea5d51e433afabbc0947637
Dr Peeper <pepperpr86@gmail.com>
2024-05-03 16:34:12 +0700

    Gun batch 3-05-2024
    - robo's second gun
    - fangz's second gun
    - shephard's gun

---
+++ fangz2/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\fangz2\sources"
+ren "H:\portalguns\fangz2\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\fangz2\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
---
+++ shephard/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\shephard\sources"
+ren "H:\portalguns\shephard\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\shephard\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
