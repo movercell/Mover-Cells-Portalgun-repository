f9da4d2e8a7e9e1033ee8f044bad37b62d3c88bb
Dr Peeper <pepperpr86@gmail.com>
2024-05-05 17:49:55 +0700

    added noelle's gun

---
+++ noelle/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\noelle\sources"
+ren "H:\portalguns\noelle\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\noelle\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
