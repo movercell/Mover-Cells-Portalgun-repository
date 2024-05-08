0c94c3fa902e9653c7c06de3b1abc215d67545a0
Dr Peeper <pepperpr86@gmail.com>
2024-05-08 17:35:55 +0700

    Gun batch 8-05-2024

---
+++ boring noobie/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\boring noobie\sources"
+ren "H:\portalguns\boring noobie\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\boring noobie\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
