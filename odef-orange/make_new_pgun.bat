7a65ca6721fd74029ef33d96547d3410d1360d6e
Dr Peeper <pepperpr86@gmail.com>
2024-05-10 12:23:14 +0700

    Gun batch 9-05-2024

---
+++ odef-orange/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\odef-orange\sources"
+ren "H:\portalguns\odef-orange\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\odef-orange\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
---
+++ tml/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\tml\sources"
+ren "H:\portalguns\tml\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\tml\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
