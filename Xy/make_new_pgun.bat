33c65ddaa374bd94047a84964f1a55563f2fb2a2
Dr Peeper <pepperpr86@gmail.com>
2024-05-12 15:30:58 +0700

    added Xy's gun

---
+++ Xy/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\Xy\sources"
+ren "H:\portalguns\Xy\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\Xy\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
