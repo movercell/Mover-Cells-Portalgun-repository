0fb56c947e02fd25e7098dbe0e013f00669adfed
Dr Peeper <pepperpr86@gmail.com>
2024-05-19 19:51:34 +0700

    Gun batch 19-05-2025

---
+++ kowot/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\kowot\sources"
+ren "H:\portalguns\kowot\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\kowot\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
