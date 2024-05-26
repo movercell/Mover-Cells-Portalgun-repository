7cddd7e8a39f518fe1acc2759b1050e5dee06f83
Dr Peeper <pepperpr86@gmail.com>
2024-05-26 20:45:42 +0700

    more gun

---
+++ fangz4/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\fangz4\sources"
+ren "H:\portalguns\fangz4\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\fangz4\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
