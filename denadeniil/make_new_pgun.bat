25f43bfb6c16b5f9aeebb209b4182df9aa75ff89
Dr Peeper <pepperpr86@gmail.com>
2024-05-30 14:04:00 +0700

    added denadeniil's gun

---
+++ denadeniil/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\denadeniil\sources"
+ren "H:\portalguns\denadeniil\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\denadeniil\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
