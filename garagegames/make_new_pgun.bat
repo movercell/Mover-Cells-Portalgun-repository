1f5b06296f49cf74f203bb06f3d0d396f9c97c4b
Dr Peeper <pepperpr86@gmail.com>
2024-06-03 16:20:38 +0700

    added garagegames's gun

---
+++ garagegames/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\garagegames\sources"
+ren "H:\portalguns\garagegames\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\garagegames\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
