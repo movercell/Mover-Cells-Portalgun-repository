fbf7f7d5c05d0b19ba3ba3718b4983ea53084193
Dr Peeper <pepperpr86@gmail.com>
2024-06-06 19:14:17 +0700

    added roasted's gun(s)

---
+++ roasted/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\roasted\sources"
+ren "H:\portalguns\roasted\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\roasted\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
