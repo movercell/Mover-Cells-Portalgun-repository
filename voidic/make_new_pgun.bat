3f834a01c16e9b75943d91dd0c580daeee77496b
Dr Peeper <pepperpr86@gmail.com>
2024-06-26 14:51:39 +0700

    added voidic's gun

---
+++ voidic/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\voidic\sources"
+ren "H:\portalguns\voidic\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\voidic\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
