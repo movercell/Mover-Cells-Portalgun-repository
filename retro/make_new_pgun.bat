55fd63b79c3106657ba46e894499e692d4616847
Dr Peeper <pepperpr86@gmail.com>
2024-07-02 17:06:03 +0700

    gun batch 02-07-2024

---
+++ retro/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\retro\sources"
+ren "H:\portalguns\retro\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\retro\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
