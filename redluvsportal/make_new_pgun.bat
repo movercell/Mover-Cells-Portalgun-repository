d0912a5c68094b583813d122556690f9e1e7798c
Dr Peeper <pepperpr86@gmail.com>
2024-11-06 17:45:16 +0700

    stuff

---
+++ redluvsportal/make_new_pgun.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Steam\steamapps\common\Portal 2\bin\vpk.exe" "H:\portalguns\redluvsportal\sources"
+ren "H:\portalguns\redluvsportal\sources.vpk" "pak02_dir.vpk"
+move /y "H:\portalguns\redluvsportal\pak02_dir.vpk" "C:\Program Files (x86)\Steam\steamapps\common\Portal 2\portal2_dlc2\"
+pause
